bcc9e3f4b003f55f5508b0bb04e6b0bba1ccde3a
Alonzo Moses <alonzo.k.moses@gmail.com>
2018-01-11 12:51:52 -0500

    modified

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOver = require('method-override')
 const pokies = require('./controller/pokies')
-// const mongoose = require('mongoose')
 const app = express()
 
 app.use(methodOver('_method'))
 app.use('/assets', express.static('public'))
-// app.use(bodyParser.json()) //handles json post requests
+app.use(bodyParser.json()) //handles json post requests
 app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
 
 app.set('port', process.env.PORT || 6009)
@@ -25,16 +24,3 @@ app.use('/', pokies)
 app.listen(app.get('port'), () => {
   console.log('For the Horde!')
 })
-
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
-
-// app.post("/", (req, res) => {
-//   res.render("app-welcome", {
-//     // title: req.body.title,
-//     // description: req.body.description,
-//     instructions: req.body.instructions,
-//     ingredients: req.body.ingredients
-//   })
-// })
